8020742f6462cbf07ea5919d8f4a449fccc341cb
Max Perez <maaaaax@mac.com>
2018-02-27 10:41:25 -0800

    Add files via upload

---
+++ Chapter 1 Exercises .cpp
@@ -0,0 +1,71 @@
+//
+//  main.cpp
+//  Chapter 1 Exercises
+//
+//  Created by Max Perez on 2/26/18.
+//  Copyright © 2018 Max Perez. All rights reserved.
+//
+//  Self Review Exercises
+//
+//  1.1
+//      a) GPS devices use a network of satellites to retrieve location-based information.
+//      b) Every year or two, the capacities of computers have appx. doubled inexpensively. This statement is known as Moore's Law.
+//      c) A database is an electronic collection of data that's organized for easy access and manipulation.
+//      d) Interpreter programs were developed to execute high-level language programs directly, without the need for compilation.
+//      e) Translator programs called compilers convert high-level language programs into machine language.
+//      f) Alpha versions are often buggy, incomplete and unstable and are released to a relatively small number of developers for testing new features, getting early feedback, etc.
+//      g) Computing costs are dropping dramatically, owing to rapid developments in hardware and software technologies.
+//
+//  1.2
+//      a) C++ source code filenames often end with the .cpp, .cxx, .cc, .C extensions.
+//      b) C++ offers software packages for Microsoft Windows such as Microsoft Visual.
+//      c) The compiler tramslates the C++ program into machine-language code-also referred to objective code.
+//      d) There is also a standard error system stream referred to as cerr.
+//
+//  1.3
+//      a) Objects have the property of information hiding...
+//      b) In C++, a program unit called clas is created to house the set of functions that perform thr class's tasks-these are known as the class's member functions.
+//      c) Programming in languages like C++, is known as object-based programming (OOP).
+//      d) With inheritance, new classes of objects are derived by absorbing characteristics of existing classes, then adding unique characteristscs of their own.
+//      e) The Unified Modeling Language, is a graphical language that allows people who design software systems to use an industry-standard notation to represent them.
+//      f) The size, shape, color and weight of an object are considered attributes of the object's class.
+//
+//  1.4
+//      a) Motion and orientation information from an accelerometer and position data from a GPS device are examples of newer forms of time-stamped signal data.
+//      b) The computer's language is the set of all the characters used to write programs and represent data items.
+//      c) Assembly Language is a type of computer language that uses English-like abbreviations for machine-language instructions.
+//      d) Output unit is a logical unit of the computer language that sends inforation which has already been processed by the computer to various devices so that it may be used outside the computer.
+//      e) The inputunit is a logical unit of the computer that retains processed information unit it can be placed on output devices by the output unit.
+//      f) Arithmetic and logic unit is a logical unit of the computer that preforms calculations and makes logical decisions.
+//      g) The speed of a computer processor can be measured by the number of bites it performs per second.
+//      h) Pascal languages are the most concenient to the programmer for writing programs quickly and easily.
+//      i) The only language a computer can directly understand is that computer's machine code.
+//      j) The CPU is the logical unit of the computer that coordinates the activities of all the other logical units.
+//
+//  1.7
+//      Watch:
+//          Object: The object is the watch and all of its built in characteristics.
+//          Attributes: The attributes of the watch are the three hands and time ticks for every quarter hour.
+//          Behaviors: As time passes, variables are changed.
+//          Class: There would be three classes. A second class which moves the second hand once every second. The minute       class which moved the minute hand once every 60 seconds. And the hour hand which moves once every hour.
+//          Inheritance: Different types of watches.
+//          Encapsulation: If the watch were to store date values, encapsulation would hide these values from say the time tracker.
+//
+//  1.9
+//      Test Drive BMI: Body Mass Index Calculator *Check*
+//
+//  1.10
+//      Hybrid car attributes: all hybrids have excellent city gas mileage along with highway gas mileage. Most hybrid cars raange from 50-60 city mpg, and 60-70 highway.
+//
+//  1.11
+//      By using statements such as "if" and "else" whenever a non-gender neutral word is crossed, we can replaced those words with a more gender neutral term. EX. " If (string word == wife) { word.wife = spouse } or something like that. We can then have the algorithm procedurally read down the paragraph. If an "if" statement is triggered, then the word will be replaced.
+//
+//  1.12
+//      This set of information will then no longer become confidential, since the employees of the company themselves are hacking the database. These employees will be able to easily disclose vital information which would jepordize millions of people's email varification methods.
+//
+//  1.13
+//      A software bug in a program which influenced the consumer's social life and security could cause a chain reacition of fallout. If the security in the program were to fail, and the credentials of the consusmers were leaked to the general public, then the confidentiality of the consumers would be broken. This could lead to various problems involving conflict between users who dont share the same ideology. Users could then take advantage of this leaked information which could result in a trust collapse of the program itself - resulting in the future distrust of the program from then-on-out.
+//
+//  1.14
+//      World hunger is generally left unoticed. People enjoy being ignorant to the reality that millions a day are struggling to get a decent meal. This problem could be easily addressed through the World Wide Web. News outlets or public webpages could simply put banners onto their websites, informing the visitors of the probelms of world hunger. Although the idea of "ads" is unfavored by most consumers, the idea is to benifit and improve the lives of those who are less fortunate.
+
